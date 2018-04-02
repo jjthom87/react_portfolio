deb3a04419a1147c5d0994e4fb8e904075e392a5
Jared Thomas <jjthom87@yahoo.com>
2018-04-02 15:01:18 -0400

    adding

--- client/app/components/pages/Blog.js
+++ client/app/components/pages/Blog.js
@@ -67,7 +67,8 @@ export default class BlogPage extends Component {
     componentDidMount(){
       let stuff;
       let that = this;
-      var socket = io.connect('https://dry-beach-37904.herokuapp.com');
+      let url = process.env.PORT ? 'https://dry-beach-37904.herokuapp.com' : 'http://localhost:3000';
+      var socket = io.connect(url);
       socket.on('messages', function (data) {
           that.setState({
             messages: data
